387e9326c7aa8a083af13070a01de1ad2e370373
paydesmond <123torgbe@gmail.com>
2024-07-23 18:20:16 +0000

    fixed error in comment.model.js

--- models/comments.model.js
+++ models/comments.model.js
@@ -3,17 +3,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const commentSchema = new Schema({
-    user_id: { type:Schema.type.ObjectId, required: true, ref:'User' },
-    folderId:{type:Schema.type.ObjectId,ref:'Directory'},
-    comment:{type:String, required:true}
-},{
-    timestamps:true
-}
-);
-
-
+    user_id: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    folderId: { type: Schema.Types.ObjectId, ref: 'Directory' },
+    comment: { type: String, required: true }
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('Comment', commentSchema);
-
-
-
