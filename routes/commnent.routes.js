0b74c1076b0cb9ef3f92b2170074ce429700ae0c
paydesmond <123torgbe@gmail.com>
2024-07-27 11:33:28 +0000

    modified comment route

--- routes/commnent.routes.js
+++ routes/commnent.routes.js
@@ -3,7 +3,7 @@ const { getComment } = require('../controllers/controller.comment/getComments')
 const router = require('express').Router()
 const ProtectStudentRoute = require('../middleware/studentProtectRoute')
 
-router.post('/:reference_Id/send',ProtectStudentRoute,createComment)
-router.get('/:reference_Id/:directoryId/get-comment',ProtectStudentRoute,getComment)
+router.post('/send',ProtectStudentRoute,createComment)
+router.get('/:directoryId/get-comment',ProtectStudentRoute,getComment)
 
 module.exports = router;
