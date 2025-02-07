01aa65e497e72851e1ec9b5483baa9b1422bc3a3
Torgbe-Desmond <123torgbe@gmail.com>
2025-02-06 13:03:19 -1200

    added SuccessResponse

--- domains/authentication/controllers/delete.js
+++ domains/authentication/controllers/delete.js
@@ -11,8 +11,9 @@ const {
 } = require("../../../utils/FirebaseInteractions");
 const ErrorHandler = require("../../../Errors/ErrorHandler");
 const asyncHandler = require("express-async-handler");
-const SuccessResponse = require("../../../utils/successResponse");
 const Handler = new ErrorHandler();
+const SuccessResponse = require("../../../utils/successResponse");
+
 
 const deleteUser = asyncHandler(async (req, res) => {
   const user_id = req.user;
